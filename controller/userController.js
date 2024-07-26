004e0c2b4118b0dc8b4f9f3a1455a707a7c58aa4
macn1 <athulraihan27@gmail.com>
2024-07-26 20:53:26 +0530

    server?

--- controller/userController.js
+++ controller/userController.js
@@ -48,14 +48,17 @@ const login = async(req,res)=>{
         
         if (userExist) {
 
-            
+            console.log("user");
 
             const isMatch = await userExist.comparePassword(password)
 
 
             if (!isMatch) {
+                console.log("ismatch");
                 res.status(400).json({message:'incorrect password'})                
             }
+
+            console.log("asd");
 
             const token = jwt.sign({id:userExist._id},process.env.secretKey)
             console.log(token);
@@ -66,6 +69,7 @@ const login = async(req,res)=>{
 
         
     } catch (error) {
+        console.log("is error");
         res.status(500).json({error:error.message})
     }
 }
